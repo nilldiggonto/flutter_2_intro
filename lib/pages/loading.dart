9954327c44d867b10ce59f8070f4271484dc1b2f
nilldiggonto <nilldiggonto@gmail.com>
2021-05-26 02:48:24 +0600

    Timer Api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart';
+// import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -7,20 +8,6 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    // print('$username- $bio');
-    http.Response response = await http
-        .get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
-    print(response.body);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    // print('init state');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ lib/services/world_time.dart
@@ -0,0 +1,9 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  String? location;
+  String? time;
+  String? flag;
+  String? url;
+}
